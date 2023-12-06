165fd22e3aaf0d59bb9e24590a2db8101a94480d
Akemi <124843821+Akemi2005@users.noreply.github.com>
2023-12-06 22:58:48 +0200

    Update script.js

--- WEBjs/PR_6/script/script.js
+++ WEBjs/PR_6/script/script.js
@@ -1,4 +1,4 @@
-const requestURL = '';
+const requestURL = 'https://github.com/Akemi2005/WebHM/blob/main/WEBjs/PR_6/json/file.json';
 const header = document.querySelector('header')
 const section = document.querySelector('section')
 const request = new XMLHttpRequest();
